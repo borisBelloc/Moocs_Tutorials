b2b05810b8ca99ce0b332abeb9afe2bac298ba50
Boris BELLOC <bb.ItWork@gmail.com>
2019-03-28 16:19:54 +0100

    continue work

--- OpenClassRoom/ReactNative-Developpez_une_application_mobile_React_Native/Cours/Moviesb2A/Components/FilmDetail.js
+++ OpenClassRoom/ReactNative-Developpez_une_application_mobile_React_Native/Cours/Moviesb2A/Components/FilmDetail.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { StyleSheet, View, ActivityIndicator } from 'react-native'
+import { StyleSheet, View, Text, ActivityIndicator, ScrollView } from 'react-native'
 import { getFilmDetailFromApi } from '../API/TMDBApi'
 
 class FilmDetail extends React.Component {
@@ -13,9 +13,18 @@ class FilmDetail extends React.Component {
     }
   }
 
+  componentDidMount() {
+    // console.log("Component FilmDetail monté")
+    getFilmDetailFromApi(this.props.navigation.state.params.idFilm).then(data => {
+      this.setState({
+        film: data,
+        isLoading: false
+      })
+    })
+  }
+
   _displayLoading() {
-    if (this.state.isLoading) {
-      // Si isLoading vaut true, on affiche le chargement à l'écran
+    if (this.state.isLoading) { // Si isLoading vaut true, on affiche le chargement à l'écran
       return (
         <View style={styles.loading_container}>
           <ActivityIndicator size='large' />
@@ -24,24 +33,32 @@ class FilmDetail extends React.Component {
     }
   }
 
-  componentDidMount() {
-    console.log("Component FilmDetail monté")
+  _displayFilm() {
+    if (this.state.film != undefined) { // on s'assure qu'un film a été choisi
+      return (
+        <ScrollView style={styles.scrollview_container}>
+          <Text>{this.state.film.title}</Text>
+          {/* Pour l'instant je n'affiche que le titre, je vous laisserais le soin de créer la vue. Après tout vous êtes aussi là pour ça non ? :)*/}
+        </ScrollView>
+      )
+    }
   }
 
 
-  render() {
-    console.log("Component FilmDetail rendu")
-    // console.log(this.props.navigation)
-    // const idFilm = this.props.navigation.state.params.idFilm // on récupere l'idFilm que l'on a fait passer ligne62 de search.js
-    // comment trouver le chemin : https://openclassrooms.com/fr/courses/4902061-developpez-une-application-mobile-react-native/5046301-concevez-une-navigation-entre-vos-vues#/id/r-5046484
-    return (
-      <View style={styles.main_container}>
-        {this._displayLoading()}
-        {/* on peut également mettre directement le chemin au lieu de passer par une variable : */}
-        {/* <Text>Détail du film { this.props.navigation.state.params.idFilm }</Text> */}
-      </View>
-    )
-  }
+render() {
+  console.log("Component FilmDetail rendu")
+  // console.log(this.props.navigation)
+  // const idFilm = this.props.navigation.state.params.idFilm // on récupere l'idFilm que l'on a fait passer ligne62 de search.js
+  // comment trouver le chemin : https://openclassrooms.com/fr/courses/4902061-developpez-une-application-mobile-react-native/5046301-concevez-une-navigation-entre-vos-vues#/id/r-5046484
+  return (
+    <View style={styles.main_container}>
+      {this._displayLoading()}
+      {this._displayFilm()}
+      {/* on peut également mettre directement le chemin au lieu de passer par une variable : */}
+      {/* <Text>Détail du film { this.props.navigation.state.params.idFilm }</Text> */}
+    </View>
+  )
+}
 }
 
 const styles = StyleSheet.create({
@@ -56,6 +73,9 @@ const styles = StyleSheet.create({
     bottom: 0,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  scrollview_container: {
+    flex: 1
   }
 })
 
