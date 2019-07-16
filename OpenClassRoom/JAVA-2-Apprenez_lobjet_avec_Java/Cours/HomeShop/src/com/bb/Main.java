0546e408ee78a2f62807b89136f150e5315f21aa
Boris BELLOC <bb.ItWork@gmail.com>
2019-07-16 17:07:39 +0200

    JAVA 2 cours ok, heritage, abstract, bill

--- OpenClassRoom/JAVA-2-Apprenez_lobjet_avec_Java/Cours/HomeShop/src/com/bb/Main.java
+++ OpenClassRoom/JAVA-2-Apprenez_lobjet_avec_Java/Cours/HomeShop/src/com/bb/Main.java
@@ -1,8 +1,23 @@
 package com.bb;
 
+import com.bb.homeshop.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Product cafe = new Product("Philips", "Senseo Noir", 79.99);
+        Television tv = new Television("Tv Samsung", "Smart TV LED 49\"", 599, 49,"LED");
+        Fridge fridge = new Fridge("BEKQ", "réfrigérateur 130L blanc", 189, 130, false);
+
+        Customer customerOne = new Customer("Boris BELLOC", "136 rue tam, Paris");
+
+        Bill bill = new Bill(customerOne);
+        bill.addProduct(cafe, 1);
+        bill.addProduct(tv, 1);
+        bill.addProduct(fridge, 1);
+
+
+
     }
 }
--- OpenClassRoom/JAVA-2-Apprenez_lobjet_avec_Java/Cours/HomeShop/src/com/bb/homeshop/Bill.java
+++ OpenClassRoom/JAVA-2-Apprenez_lobjet_avec_Java/Cours/HomeShop/src/com/bb/homeshop/Bill.java
@@ -1,10 +1,12 @@
 package com.bb.homeshop;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Bill {
     private Customer customer;
-    private Map<Product, Integer> products;
+    // par défault, une map (ou tout type objet) est null, il faut ajouter une valeur a l'interieur pour qu'il ne soit plus nul, soit ; 'new HashMap'
+    private Map<Product, Integer> products = new HashMap<Product, Integer>();
 
     // Constructeur de l'objet ; la methode porte le nom de la class
     // pas de type de retour
@@ -14,7 +16,8 @@ public class Bill {
 
     /**
      * Add a product with a quantity in the bill
-     * @param product product to add
+     *
+     * @param product  product to add
      * @param quantity quantity of the product
      */
     public void addProduct(Product product, Integer quantity) {
