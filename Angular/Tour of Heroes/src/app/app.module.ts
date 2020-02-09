d105ca674294c76059d5b0e57918a0e9b14f1e9e
Boris BELLOC <bb.itwork@gmail.com>
2020-02-09 16:16:22 +0100

    save before new service

--- Angular/Tour of Heroes/src/app/app.module.ts
+++ Angular/Tour of Heroes/src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+
 // ** Make HttpClient available everywhere in the app in two steps.
 // First, add it to the root AppModule by importing it:
 import { HttpClientModule } from '@angular/common/http';
 
+// In-memory Web API
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-
 import { FormsModule } from '@angular/forms';
+import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component'; // <-- NgModel lives here
@@ -28,6 +31,13 @@ import { DashboardComponent } from './dashboard/dashboard.component'; // <-- NgM
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false
+    })
   ],
   providers: [
     // no need to place any providers due to the `providedIn` flag...
