cf0731eb1aa5b8472baee546892c34c795990f94
Boris BELLOC <bb.itwork@gmail.com>
2020-02-23 18:14:26 +0100

    §3 ok; ngStyle and ngClass

--- Angular/ocrAng5/src/app/app.component.ts
+++ Angular/ocrAng5/src/app/app.component.ts
@@ -8,20 +8,28 @@ import { Component } from '@angular/core';
 export class AppComponent {
   isAuth = false;
 
-  appareilOne = 'Machine à laver';
-  appareilTwo = 'Télévision';
-  appareilThree = 'Ordinateur';
+  appareils = [
+    {
+      name: 'Machine à laver',
+      status: 'éteint'
+    },
+    {
+      name: 'Frigo',
+      status: 'allumé'
+    },
+    {
+      name: 'Ordinateur',
+      status: 'éteint'
+    }
+  ];
 
   constructor() {
-    setTimeout(
-      () => {
-        this.isAuth = true;
-      }, 2000
-    );
+    setTimeout(() => {
+      this.isAuth = true;
+    }, 2000);
   }
 
   onAllumer() {
     console.log('On allume tout! ');
-
   }
 }
--- Angular/ocrAng5/src/app/appareil/appareil.component.ts
+++ Angular/ocrAng5/src/app/appareil/appareil.component.ts
@@ -6,17 +6,22 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./appareil.component.scss']
 })
 export class AppareilComponent implements OnInit {
-
   @Input() appareilName: string;
   @Input() appareilStatus: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   getStatus() {
     return this.appareilStatus;
   }
 
+  getColor() {
+    if (this.appareilStatus === 'allumé') {
+      return 'green';
+    } else if (this.appareilStatus === 'éteint') {
+      return 'red';
+    }
+  }
 }
