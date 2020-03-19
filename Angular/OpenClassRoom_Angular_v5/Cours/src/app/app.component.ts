c3f872b6288d4b1e2cb5cf1450897a372a6aa063
Boris BELLOC <bb.itwork@gmail.com>
2020-03-19 18:28:51 +0100

    observe the observable

--- Angular/OpenClassRoom_Angular_v5/Cours/src/app/app.component.ts
+++ Angular/OpenClassRoom_Angular_v5/Cours/src/app/app.component.ts
@@ -1,11 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { AppareilService } from './services/appareil.service';
+import { Observable } from 'rxjs';
+
+import { interval } from 'rxjs';
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  secondes: number;
+
   constructor() {}
+
+
+  ngOnInit(): void {
+    const counter = interval(1000);
+    // On observe l'observable :
+    counter.subscribe(
+      (value: number) => {
+        this.secondes = value;
+      },
+      (error: any) => {
+        console.log('Une erreur a été rencontrée !');
+      },
+      () => {
+        console.log('Observable complétée !');
+      }
+    );
+  }
 }
