a316070974a869512dd2ede4c59da1cb357543fb
Boris BELLOC <bb.itwork@gmail.com>
2020-05-09 14:51:24 +0200

    p1 ok

--- Chrome_Extensions/official_tuto_one/background.js
+++ Chrome_Extensions/official_tuto_one/background.js
@@ -1,5 +1,17 @@
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
+    console.log('The color is green.');
+  });
+
+
+  // page_action is declared in the manifest, it is up to the extension to tell the browser when the user can interact with popup.html :
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: 'developer.chrome.com'},
+      })
+      ],
+          actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
   });
 });
